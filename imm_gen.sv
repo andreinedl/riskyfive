b7dec021d9d89bcecf7bd60d26d9bdf389d029aa
Andrei Nedelcu <andreinedelcu48@gmail.com>
2026-09-17 15:33:29 +0300

    fix static opcode assignment to immediate generator

--- imm_gen.sv
+++ imm_gen.sv
@@ -5,7 +5,8 @@ module imm_gen(
     output logic [32 - 1:0] imm_o
 );
 
-opcode_e opcode = opcode_e'(instr_i[6:0]);
+opcode_e opcode;
+assign opcode = opcode_e'(instr_i[6:0]);
 
 always_comb
     case (opcode)
